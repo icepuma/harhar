c7c86c3f8f29fe73655cd170c9a2da0bc599d1eb
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2023-11-25 17:03:06 +0100

    fix: tests by adding a noop test

--- check.sh
+++ check.sh
@@ -5,3 +5,4 @@ set -e -u -o pipefail
 dart run build_runner build
 dart format .
 dart fix --apply
+dart test
---
+++ test/noop_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('make the test work', () {
+    expect(true, true);
+  });
+}
